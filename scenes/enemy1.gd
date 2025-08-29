c296d1088110218a471bb37948874863390941a9
Akshat1401 <akshat.hemant@anywhere.co>
2025-08-29 20:20:31 +0530

    added roll animation

--- scenes/enemy1.gd
+++ scenes/enemy1.gd
@@ -6,19 +6,19 @@ var player = false
 
 
 func _ready() -> void:
-	print(1)
-	
+    print(1)
+    
 func _physics_process(delta):
-	if player and player is CharacterBody2D:
-		if player.global_position.x < global_position.x:
-			sprite.flip_h = true
-			sprite.offset.x = -20   # face left
-		else:
-			sprite.flip_h = false
-			sprite.offset.x = 20  # face right
+    if player and player is CharacterBody2D:
+        if player.global_position.x < global_position.x:
+            sprite.flip_h = true
+            sprite.offset.x = -20   # face left
+        else:
+            sprite.flip_h = false
+            sprite.offset.x = 20  # face right
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	player = body
+    player = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	player = false
+    player = false
